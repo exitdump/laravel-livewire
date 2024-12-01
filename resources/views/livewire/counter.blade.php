5377ec1050b5e8972c538321d567b8a027be9446
exitdump <jahidhasan100101@gmail.com>
2024-12-01 18:39:47 +0600

    init

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+   <div class="flex gap-5 mx-auto">
+    <button wire:click="decrement" class="bg-gray-500 text-white">Decrement</button>
+    {{$counter}}
+    <button wire:click="increment" class="bg-gray-500 text-white">Increment</button>
+   </div>
+</div>
