d425197f9546887cab46c87e995e1e4963b8f4f5
exitdump <jahidhasan100101@gmail.com>
2024-12-04 14:50:04 +0600

    add likes entity with eloquent relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,14 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, "user_id");
     }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class,"user_id");
+    }
+
+    public function isLikedBy(User $user)
+    {
+        return $this->likes()->where("user_id", $user->id)->exists();
+    }
 }
