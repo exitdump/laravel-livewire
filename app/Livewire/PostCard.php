d8865c39ff99f8d4f0edb07700cdc62d46cfe38d
exitdump <jahidhasan100101@gmail.com>
2024-12-05 18:31:48 +0600

    add profile page

--- app/Livewire/PostCard.php
+++ app/Livewire/PostCard.php
@@ -4,11 +4,13 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Livewire\Attributes\On;
 
 class PostCard extends Component
 {
     public Post $post;
 
+    #[On('post-created')]
     public function mount(Post $post)
     {
         $this->post = $post;
